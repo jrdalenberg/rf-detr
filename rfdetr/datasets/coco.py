c76f9a657bde694e7c0dafb1c5fe33b5cfee4426
Jelle Dalenberg <j.r.dalenberg@gmail.com>
2026-01-14 15:53:34 +0100

    Add vertical flips to training

--- rfdetr/datasets/coco.py
+++ rfdetr/datasets/coco.py
@@ -158,6 +158,7 @@ def make_coco_transforms(image_set, resolution, multi_scale=False, expanded_scal
     if image_set == 'train':
         return T.Compose([
             T.RandomHorizontalFlip(),
+            T.RandomVerticalFlip(),
             T.RandomSelect(
                 T.RandomResize(scales, max_size=1333),
                 T.Compose([
@@ -204,6 +205,7 @@ def make_coco_transforms_square_div_64(image_set, resolution, multi_scale=False,
     if image_set == 'train':
         return T.Compose([
             T.RandomHorizontalFlip(),
+            T.RandomVerticalFlip(),
             T.RandomSelect(
                 T.SquareResize(scales),
                 T.Compose([
